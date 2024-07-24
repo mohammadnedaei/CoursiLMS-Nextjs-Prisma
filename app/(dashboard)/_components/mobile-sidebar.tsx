02171c5d5e7cc142045cbe78de47095de40c85fd
mohammadnedaei <nedaei2002@gmail.com>
2024-07-25 00:48:33 +0330

    feat(navbar): add mobile drawer sheet

--- app/(dashboard)/_components/mobile-sidebar.tsx
+++ app/(dashboard)/_components/mobile-sidebar.tsx
@@ -1,7 +1,16 @@
 import {Menu} from "lucide-react";
+import {Sheet, SheetContent, SheetTrigger} from "@/components/ui/sheet";
+import {Sidebar} from "@/app/(dashboard)/_components/sidebar";
 
 export const MobileSidebar = () => {
     return (
-        <Menu/>
+        <Sheet>
+            <SheetTrigger className="md:hidden pr-4 hover:opacity-75 transition">
+                <Menu/>
+            </SheetTrigger>
+            <SheetContent side="left" className="p-0">
+                <Sidebar/>
+            </SheetContent>
+        </Sheet>
     )
 }
