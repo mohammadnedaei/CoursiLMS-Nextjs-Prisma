5623211c6b2e21f99804cc0bb7a3e3d8f3a19c4a
mohammadnedaei <nedaei2002@gmail.com>
2024-07-24 22:36:46 +0330

    feat(sign-out): add sign-out & sign-in sign-up interactivity

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image'
-import {Button} from "@/components/ui/button";
+import {UserButton} from "@clerk/nextjs";
 
 export default function Home() {
-  return (
-    <>
-      <Button>Hello</Button>
-    </>
-  )
+    return (
+        <>
+            <UserButton afterSignOutUrl="/"/>
+        </>
+    )
 }
