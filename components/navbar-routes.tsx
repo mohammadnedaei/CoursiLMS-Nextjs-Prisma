498079915e9fa7bd8680a035796b92198265c7f8
mohammadnedaei <nedaei2002@gmail.com>
2024-07-25 01:36:14 +0330

    feat(navbar): add logout and teacher mode

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,10 +1,32 @@
 "use client";
 import {UserButton} from "@clerk/nextjs";
+import {usePathname, useRouter} from "next/navigation";
+import {Button} from "@/components/ui/button";
+import {LogOut} from "lucide-react";
+import Link from "next/link";
 
 export const NavbarRoutes = () => {
+    const pathname = usePathname()
+    const router = useRouter()
+
+    const isTeacherPage = pathname?.startsWith("/teacher")
+    const isPlayerPage = pathname?.includes("/chapter")
+
     return (
         <div className="flex gap-x-2 ml-auto">
-            <UserButton/>
+            {isTeacherPage || isPlayerPage ? (
+                <Button size="sm" variant="ghost">
+                    <LogOut className="h-4 w-4 mr-2"/>
+                    Exit
+                </Button>
+            ) : (
+                <Link href="/teacher/courses">
+                    <Button size="sm" variant="ghost">
+                        Teacher Mode
+                    </Button>
+                </Link>
+            )}
+            <UserButton afterSignOutUrl="/"/>
         </div>
     )
 }
