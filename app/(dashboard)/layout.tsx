2b420b70aa98eccfe1454ae86cb06adaa5a7dac1
mohammadnedaei <nedaei2002@gmail.com>
2024-07-25 02:11:55 +0330

    feat(dashboard): fixed layout style

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -12,7 +12,7 @@ const DashboardLayout = ({children}: {
             <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
                 <Sidebar/>
             </div>
-            <main className="md:pl-56">
+            <main className="md:pl-56 pt-[80px] h-full">
                 {children}
             </main>
         </div>
