a84951eb4a6714b616e0e009b3a8b789ddcf6c20
mohammadnedaei <nedaei2002@gmail.com>
2024-07-24 23:24:35 +0330

    feat(sidebar): sidebar item props type mismatch fix

--- app/(dashboard)/_components/sidebar-item.tsx
+++ app/(dashboard)/_components/sidebar-item.tsx
@@ -1,6 +1,14 @@
+import {LucideIcon} from "lucide-react";
+
 "use-client";
 
-export const SidebarItem = () => {
+interface SidebarItemProps {
+    icon: LucideIcon;
+    label: string;
+    href: string;
+}
+
+export const SidebarItem = ({icon: Icon, label, href,}: SidebarItemProps) => {
     return (
         <div>
             s
