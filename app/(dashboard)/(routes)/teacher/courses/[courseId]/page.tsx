bcecefba15528e52b133e1798cc581ea63a93be8
mohammadnedaei <nedaei2002@gmail.com>
2024-07-27 11:14:34 +0330

    feat(dashboard): added course id page

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -0,0 +1,8 @@
+const CourseIdPage = ({params}: { params: { courseId: string } }) => {
+    return (
+        <div>
+            {params.courseId}
+        </div>
+    )
+}
+export default CourseIdPage
