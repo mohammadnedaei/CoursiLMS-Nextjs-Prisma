dbab469dfd0ed12ef1eeeea4da89986c31fea653
mohammadnedaei <nedaei2002@gmail.com>
2024-07-27 21:58:16 +0330

    api(upload): added uploadthing configuration for photo video and pdf

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,30 @@
+import {createUploadthing, type FileRouter} from "uploadthing/next";
+import {UploadThingError} from "uploadthing/server";
+import {auth} from "@clerk/nextjs";
+
+const f = createUploadthing();
+
+const handleAuth = () => {
+    const {userId} = auth()
+    console.log(userId)
+    if (!userId) throw new Error("Unauthorized");
+    return {userId}
+}
+
+
+export const ourFileRouter = {
+    courseImage: f({image: {maxFileSize: "4MB", maxFileCount: 1}})
+        .middleware(() => handleAuth())
+        .onUploadComplete(() => {
+        }),
+    courseAttachment: f(["text", "image", "video", "audio", "pdf"])
+        .middleware(() => handleAuth())
+        .onUploadComplete(() => {
+        }),
+    courseVideo: f({video: {maxFileCount: 1, maxFileSize: "512GB"}})
+        .middleware(() => handleAuth())
+        .onUploadComplete(() => {
+        })
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
