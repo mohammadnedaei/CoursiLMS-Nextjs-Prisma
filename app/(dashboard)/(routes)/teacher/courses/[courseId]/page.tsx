fb1b32d30e2f375498979c7745200ced06753ad2
mohammadnedaei <nedaei2002@gmail.com>
2024-07-27 11:26:52 +0330

    feat(dashboard): added course setup info

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,7 +1,55 @@
-const CourseIdPage = ({params}: { params: { courseId: string } }) => {
+import {db} from "@/lib/db";
+import {auth} from "@clerk/nextjs";
+import {redirect} from "next/navigation";
+
+const CourseIdPage = async ({params}: { params: { courseId: string } }) => {
+    const {userId} = auth()
+    if (!userId) {
+        return redirect("/")
+    }
+
+    const course = await db.course.findUnique({
+        where: {
+            id: params.courseId
+        }
+    })
+
+    if (!course) {
+        return redirect("/")
+    }
+
+    const requiredFields = [
+        course.title,
+        course.description,
+        course.imageUrl,
+        course.price,
+        course.categoryId
+    ];
+
+    const totalFields = requiredFields.length;
+    const completedFields = requiredFields.filter(Boolean).length;
+
+    const completionText = `(${completedFields}/${totalFields})`
+
     return (
-        <div>
-            {params.courseId}
+        <div className="p-6">
+            <div className="flex items-center justify-between">
+                <div className="flex flex-col gap-y-2">
+                    <h1 className="text-2xl font-medium">
+                        Course setup
+                    </h1>
+                    <span className="text-sm text-slate-700">
+                        Complete all fields {completionText}
+                    </span>
+                </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+                <div>
+                    <div className="flex items-center gap-x-2">
+                        <h2 className="text-xl">Customize your course</h2>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
