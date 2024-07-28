400addb4c414577966669110ae63ec6f3c0baf82
mohammadnedaei <nedaei2002@gmail.com>
2024-07-28 12:02:51 +0330

    feat(dashboard): created a combobox for categories

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/image-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/image-form.tsx
@@ -2,18 +2,12 @@
 
 import * as z from "zod"
 import axios from "axios";
-import {zodResolver} from "@hookform/resolvers/zod";
-import {useForm} from "react-hook-form";
 import {useRouter} from "next/navigation";
 
-import {Form, FormControl, FormField, FormItem, FormMessage} from "@/components/ui/form";
-import {Input} from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
 import {ImageIcon, Pencil, PlusCircle} from "lucide-react";
 import {useState} from "react";
 import toast from "react-hot-toast";
-import {cn} from "@/lib/utils";
-import {Textarea} from "@/components/ui/textarea";
 import {Course} from "@prisma/client";
 import Image from "next/image";
 import {FileUpload} from "@/components/file-upload";
@@ -35,15 +29,6 @@ export const ImageForm = ({initialData, courseId}: ImageFormProps) => {
     const toggleEdit = () => setIsEditing((current) => !current);
 
     const router = useRouter();
-
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            imageUrl: initialData?.imageUrl || ""
-        },
-    });
-
-    const {isSubmitting, isValid} = form.formState
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
         try {
