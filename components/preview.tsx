295346a47bf6b5c5f5ea0064d81df754ed2c2d86
mohammadnedaei <nedaei2002@gmail.com>
2024-08-03 21:41:08 +0330

    dashboard(teacher): added react quill for rich text editor

---
+++ components/preview.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import dynamic from "next/dynamic";
+
+import "react-quill/dist/quill.bubble.css"
+import {useMemo} from "react";
+
+interface PreviewProps {
+    value: string;
+};
+
+export const Preview = ({onChange, value,}: PreviewProps) => {
+    const ReactQuill = useMemo(() => dynamic(() => import("react-quill"), {ssr: false}), []);
+
+    return (
+        <ReactQuill
+            theme="bubble"
+            value={value}
+            readOnly
+        />
+    )
+}
