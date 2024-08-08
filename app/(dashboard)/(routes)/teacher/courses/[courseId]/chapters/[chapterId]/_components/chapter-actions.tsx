2626c1fca2a9b837303e4ee8add1c7cee040547f
mohammadnedaei <nedaei2002@gmail.com>
2024-08-08 19:47:25 +0330

    dashboard(teacher): chapter publish and unpublish logic + ui

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
@@ -3,6 +3,11 @@
 
 import {Button} from "@/components/ui/button";
 import {Trash} from "lucide-react";
+import {ConfirmModal} from "@/components/modals/confirm-modal";
+import {useState} from "react";
+import toast from "react-hot-toast";
+import axios from "axios";
+import {useRouter} from "next/navigation";
 
 interface ChapterActionsProps {
     disabled: boolean;
@@ -12,20 +17,61 @@ interface ChapterActionsProps {
 }
 
 export const ChapterActions = ({disabled, courseId, chapterId, isPublished}: ChapterActionsProps) => {
+    const router = useRouter()
+    const [isLoading, setIsLoading] = useState(false)
+
+    const onClick = async () => {
+        try {
+            setIsLoading(true)
+
+            if (isPublished) {
+                await axios.patch(`/api/courses/${courseId}/chapters/${chapterId}/unpublish`)
+                toast.success("Chapter unpublished")
+            } else {
+                await axios.patch(`/api/courses/${courseId}/chapters/${chapterId}/publish`)
+                toast.success("Chapter published")
+            }
+
+            router.refresh()
+        } catch {
+            toast.error("Something went wrong")
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+    const onDelete = async () => {
+        try {
+            setIsLoading(true)
+            await axios.delete(`/api/courses/${courseId}/chapters/${chapterId}`);
+
+            toast.success("Chapter deleted")
+
+            router.refresh();
+            router.push(`/teacher/courses/${courseId}`)
+        } catch {
+            toast.error("Something went wrong")
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
     return (
         <div className="flex items-center gap-x-2">
             <Button
-                onClick={() => {
-                }}
-                disabled={disabled}
+                onClick={onClick}
+                disabled={disabled || isLoading}
                 variant="outline"
                 size="sm"
             >
                 {isPublished ? "Unpublish" : "Publish"}
             </Button>
-            <Button size="sm">
-                <Trash className="h-4 w-4"/>
-            </Button>
+            <ConfirmModal onConfirm={onDelete}>
+                <Button size="sm" disabled={isLoading}>
+                    <Trash className="h-4 w-4"/>
+                </Button>
+            </ConfirmModal>
+
         </div>
     )
 }
