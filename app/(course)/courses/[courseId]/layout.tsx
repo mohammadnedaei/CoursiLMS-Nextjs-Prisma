f98b1ebf81671cb4bd88e10dd4b7cd434dc286bc
mohammadnedaei <nedaei2002@gmail.com>
2024-08-10 22:36:15 +0330

    feat(course): added sidebar + navbar

--- app/(course)/courses/[courseId]/layout.tsx
+++ app/(course)/courses/[courseId]/layout.tsx
@@ -3,6 +3,7 @@ import {redirect} from "next/navigation";
 import {db} from "@/lib/db";
 import {getProgress} from "@/actions/get-progress";
 import {CourseSidebar} from "@/app/(course)/courses/[courseId]/_components/course-sidebar";
+import {CourseNavbar} from "@/app/(course)/courses/[courseId]/_components/course-navbar";
 
 const CourseLayout = async ({children, params}: { children: React.ReactNode; params: { courseId: string } }) => {
     const {userId} = auth()
@@ -38,13 +39,19 @@ const CourseLayout = async ({children, params}: { children: React.ReactNode; par
 
     return (
         <div className="h-full">
+            <div className="h-[80px] md:pl-80 fixed inset-y-0 w-full z-50">
+                <CourseNavbar
+                    course={course}
+                    progressCount={progressCount}
+                />
+            </div>
             <div className="hidden md:flex h-full w-80 flex-col fixed inset-y-0 z-50">
                 <CourseSidebar
                     course={course}
                     progressCount={progressCount}
                 />
             </div>
-            <main className="md:pl-80 h-full">
+            <main className="md:pl-80 pt-[80px] h-full">
                 {children}
             </main>
         </div>
