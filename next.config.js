9c96c1f60ad4b1e640ae5b581ade60a97388f248
mohammadnedaei <nedaei2002@gmail.com>
2024-08-21 20:15:15 +0330

    refactor(meta): refactor meta data

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: [
-            "utfs.io"
-        ]
-    }
-}
+  images: {
+    domains: ["utfs.io", "img.clerk.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
