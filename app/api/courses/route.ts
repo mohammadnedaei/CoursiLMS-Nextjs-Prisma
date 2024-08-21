5096b4b8eb14fc9bd99691f840edaee8fc28025a
mohammadnedaei <nedaei2002@gmail.com>
2024-08-21 20:46:58 +0330

    api(courses): added author and author image to course creating api

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -1,28 +1,31 @@
-import {NextResponse} from "next/server";
-import {auth} from "@clerk/nextjs";
-import {db} from "@/lib/db";
+import { NextResponse } from "next/server";
+import { auth, clerkClient } from "@clerk/nextjs";
+import { db } from "@/lib/db";
 
-export async function POST(
-    req: Request,
-) {
-    try {
-        const {userId} = auth()
-        const {title} = await req.json()
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const userName = await clerkClient.users.getUser(userId);
+    const author = `${userName.firstName || ""} ${userName.lastName || ""}`;
+    const authorImage = userName.imageUrl;
+    const { title } = await req.json();
 
-        if (!userId) {
-            return new NextResponse("Unauthorized", {status: 401})
-        }
-
-        const course = await db.course.create({
-            data: {
-                userId,
-                title
-            }
-        })
-
-        return NextResponse.json(course)
-    } catch (error) {
-        console.log("[COURSES]", error)
-        return new NextResponse("Internal Error", {status: 500})
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
     }
+
+    const course = await db.course.create({
+      data: {
+        userId,
+        author,
+        authorImageUrl: authorImage,
+        title,
+      },
+    });
+
+    return NextResponse.json(course);
+  } catch (error) {
+    console.log("[COURSES]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
 }
