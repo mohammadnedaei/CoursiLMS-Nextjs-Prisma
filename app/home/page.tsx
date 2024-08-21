73e8fed30de1a6c3c004b080f334b64de231fe68
mohammadnedaei <nedaei2002@gmail.com>
2024-08-21 20:42:49 +0330

    landing(flow): added all landing page components

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,6 +1,26 @@
-const HomePage = () => {
-    return (
-        <div>Home Page</div>
-    )
-}
-export default HomePage
+import { db } from "@/lib/db";
+import Newsletter from "../(dashboard)/_components/newsletter";
+import Testimonials from "../(dashboard)/_components/testimonials";
+import Features from "./_components/features";
+import Hero from "./_components/hero";
+import Zigzag from "./_components/zigzag";
+import { useState } from "react";
+import { getPublicCourses } from "@/actions/get-public-courses";
+import { PublicCategories } from "./_components/public-categories";
+import { FeaturedCourse } from "./_components/featured-course";
+const HomePage = async () => {
+  const courses = await getPublicCourses();
+  const featuredCourse = courses.length > 0 ? courses[0] : null;
+  return (
+    <>
+      <Hero />
+      <Features />
+      {featuredCourse && <FeaturedCourse course={featuredCourse} />}
+      <Zigzag />
+      <PublicCategories />
+      <Testimonials />
+      <Newsletter />
+    </>
+  );
+};
+export default HomePage;
