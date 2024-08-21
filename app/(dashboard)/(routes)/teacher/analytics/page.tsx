6bb862deda9c992bbfb8be2df28a9edc47393569
mohammadnedaei <nedaei2002@gmail.com>
2024-08-21 20:41:33 +0330

    dashboard(analytics): added course analytics to teacher panel

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,6 +1,8 @@
 import { getAnalytics } from "@/actions/get-analytics";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import { DataCard } from "./_components/data-card";
+import { Chart } from "./_components/chart";
 
 const AnalyticsPage = async () => {
   const { userId } = auth();
@@ -13,9 +15,11 @@ const AnalyticsPage = async () => {
 
   return (
     <div className="p-6">
-      <div className="grid grid-cols-1">
-
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+        <DataCard label="Total Revenue" value={totalRevenue} shouldFormat />
+        <DataCard label="Total Sales" value={totalSales} />
       </div>
+      <Chart data={data} />
     </div>
   );
 };
