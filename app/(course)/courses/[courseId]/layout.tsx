37ebdf0c6493c6ae14584ce0f59bb0945c949946
mohammadnedaei <nedaei2002@gmail.com>
2024-08-21 19:37:32 +0330

    course(certificate): added add certificate if progress is 100

--- app/(course)/courses/[courseId]/layout.tsx
+++ app/(course)/courses/[courseId]/layout.tsx
@@ -1,60 +1,80 @@
-import {auth} from "@clerk/nextjs";
-import {redirect} from "next/navigation";
-import {db} from "@/lib/db";
-import {getProgress} from "@/actions/get-progress";
-import {CourseSidebar} from "@/app/(course)/courses/[courseId]/_components/course-sidebar";
-import {CourseNavbar} from "@/app/(course)/courses/[courseId]/_components/course-navbar";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
+import { getProgress } from "@/actions/get-progress";
+import { CourseSidebar } from "@/app/(course)/courses/[courseId]/_components/course-sidebar";
+import { CourseNavbar } from "@/app/(course)/courses/[courseId]/_components/course-navbar";
+import axios from "axios";
+import { addCertificate } from "@/actions/add-certificate";
 
-const CourseLayout = async ({children, params}: { children: React.ReactNode; params: { courseId: string } }) => {
-    const {userId} = auth()
+const CourseLayout = async ({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { courseId: string };
+}) => {
+  const { userId } = auth();
 
-    if (!userId) return redirect("/")
+  if (!userId) return redirect("/");
 
-    const course = await db.course.findUnique({
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+    },
+    include: {
+      chapters: {
         where: {
-            id: params.courseId,
+          isPublished: true,
         },
         include: {
-            chapters: {
-                where: {
-                    isPublished: true,
-                },
-                include: {
-                    userProgress: {
-                        where: {
-                            userId,
-                        }
-                    }
-                },
-                orderBy: {
-                    position: "asc",
-                }
-            }
-        }
-    });
+          userProgress: {
+            where: {
+              userId,
+            },
+          },
+        },
+        orderBy: {
+          position: "asc",
+        },
+      },
+    },
+  });
 
-    if (!course) return redirect("/")
+  if (!course) return redirect("/");
 
-    const progressCount = await getProgress(userId, course.id)
+  const progressCount = await getProgress(userId, course.id);
 
-    return (
-        <div className="h-full">
-            <div className="h-[80px] md:pl-80 fixed inset-y-0 w-full z-50">
-                <CourseNavbar
-                    course={course}
-                    progressCount={progressCount}
-                />
-            </div>
-            <div className="hidden md:flex h-full w-80 flex-col fixed inset-y-0 z-50">
-                <CourseSidebar
-                    course={course}
-                    progressCount={progressCount}
-                />
-            </div>
-            <main className="md:pl-80 pt-[80px] h-full">
-                {children}
-            </main>
-        </div>
-    )
-}
+  const certificate = await db.certificate.findUnique({
+    where: {
+      userId_courseId: {
+        userId,
+        courseId: course.id,
+      },
+    },
+  });
+
+  if (progressCount === 100 && !certificate) {
+    try {
+      await addCertificate({
+        userId,
+        courseId: course.id,
+        courseTitle: course.title,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  return (
+    <div className="h-full">
+      <div className="h-[80px] md:pl-80 fixed inset-y-0 w-full z-50">
+        <CourseNavbar course={course} progressCount={progressCount} />
+      </div>
+      <div className="hidden md:flex h-full w-80 flex-col fixed inset-y-0 z-50">
+        <CourseSidebar course={course} progressCount={progressCount} />
+      </div>
+      <main className="md:pl-80 pt-[80px] h-full">{children}</main>
+    </div>
+  );
+};
 export default CourseLayout;
