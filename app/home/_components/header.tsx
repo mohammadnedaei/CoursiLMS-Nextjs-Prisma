64f8c4243760aca6790a05007a4f3d3cd797824d
mohammadnedaei <nedaei2002@gmail.com>
2024-08-21 21:32:47 +0330

    landing(components): added header component

---
+++ app/home/_components/header.tsx
@@ -0,0 +1,53 @@
+import Link from "next/link";
+import MobileMenu from "./mobile-menu";
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <header className="absolute w-full z-30">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="flex items-center justify-between h-20">
+          {/* Site branding */}
+          <div className="shrink-0 mr-4">
+            {/* Logo */}
+            <Link href="/home" className="block" aria-label="Cruip">
+              <div>
+                <Image
+                  width="150"
+                  height="150"
+                  src="/logo.svg"
+                  alt="logo-header"
+                />
+              </div>
+            </Link>
+          </div>
+
+          {/* Desktop navigation */}
+          <nav className="hidden md:flex md:grow">
+            {/* Desktop sign in links */}
+            <ul className="flex grow justify-end flex-wrap items-center">
+              <li>
+                <Link
+                  href="/sign-in"
+                  className="font-medium  hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out"
+                >
+                  Sign in
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/sign-up"
+                  className="p-2 rounded-md text-white bg-blue-600 hover:bg-blue-700 ml-3"
+                >
+                  Sign up
+                </Link>
+              </li>
+            </ul>
+          </nav>
+
+          <MobileMenu />
+        </div>
+      </div>
+    </header>
+  );
+}
