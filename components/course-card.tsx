1fca15e530fbb068cf70e6240659276dfc98f348
mohammadnedaei <nedaei2002@gmail.com>
2024-08-21 18:55:52 +0330

    course(card): added author to course card item

--- components/course-card.tsx
+++ components/course-card.tsx
@@ -1,58 +1,76 @@
 import Link from "next/link";
 import Image from "next/image";
-import {IconBadge} from "@/components/icon-badge";
-import {BookOpen} from "lucide-react";
-import {formatPrice} from "@/lib/format";
-import {CourseProgress} from "@/components/course-progress";
+import { IconBadge } from "@/components/icon-badge";
+import { BookOpen } from "lucide-react";
+import { formatPrice } from "@/lib/format";
+import { CourseProgress } from "@/components/course-progress";
 
 interface CourseCardProps {
-    id: string;
-    title: string;
-    imageUrl: string;
-    chaptersLength: number;
-    price: number;
-    progress: number | null;
-    category: string;
+  id: string;
+  title: string;
+  imageUrl: string;
+  chaptersLength: number;
+  price: number;
+  progress: number | null;
+  category: string;
+  author: string;
+  authorImageUrl: string;
 }
 
-export const CourseCard = ({id, title, imageUrl, chaptersLength, price, progress, category}: CourseCardProps) => {
-    return (
-        <Link href={`/courses/${id}`}>
-            <div className="group hover:shadow-sm transition overflow-hidden border rounded-lg p-3 h-full">
-                <div className="relative w-full aspect-video rounded-md overflow-hidden">
-                    <Image className="object-cover" fill src={imageUrl} alt={title}/>
-                </div>
-                <div className="flex flex-col pt-2">
-                    <div className="text-lg md:text-base font-medium group-hover:text-sky-700 transition line-clamp-2">
-                        {title}
-                    </div>
-                    <p className="text-xs text-muted-foreground">
-                        {category}
-                    </p>
-                    <div className="my-3 flex items-center gap-x-2 text-sm md:text-xs">
-                        <div className="flex items-center gap-x-1 text-slate-500">
-                            <IconBadge
-                                size="sm"
-                                icon={BookOpen}
-                            />
-                            <span>
-                                {chaptersLength} {chaptersLength === 1 ? "Chapter" : "Chapters"}
-                            </span>
-                        </div>
-                    </div>
-                    {progress !== null ? (
-                        <CourseProgress
-                            value={progress}
-                            variant={progress === 100 ? "success" : "default"}
-                            size="sm"
-                                        />
-                    ) : (
-                        <p className="text-md md:text-sm font-medium text-slate-700">
-                            {formatPrice(price)}
-                        </p>
-                    )}
-                </div>
+export const CourseCard = ({
+  id,
+  title,
+  imageUrl,
+  chaptersLength,
+  price,
+  progress,
+  category,
+  author,
+  authorImageUrl,
+}: CourseCardProps) => {
+  return (
+    <Link href={`/courses/${id}`}>
+      <div className="group hover:shadow-sm transition overflow-hidden border rounded-lg p-3 h-full hover:bg-slate-200">
+        <div className="relative w-full aspect-video rounded-md overflow-hidden">
+          <Image className="object-cover" fill src={imageUrl} alt={title} />
+        </div>
+        <div className="flex flex-col pt-2">
+          <div className="text-lg md:text-base font-medium group-hover:text-sky-700 transition line-clamp-2">
+            {title}
+          </div>
+          <p className="text-xs text-muted-foreground">{category}</p>
+          <div className="flex flex-row gap-x-3 mt-5 items-center">
+            <Image
+              src={authorImageUrl}
+              alt="author"
+              width={35}
+              height={35}
+              className="rounded-full"
+            />
+            <p className="text-sm text-gray-800">{author}</p>
+          </div>
+
+          <div className="my-3 flex items-center gap-x-2 text-sm md:text-xs">
+            <div className="flex items-center gap-x-1 text-slate-500">
+              <IconBadge size="sm" icon={BookOpen} />
+              <span>
+                {chaptersLength} {chaptersLength === 1 ? "Chapter" : "Chapters"}
+              </span>
             </div>
-        </Link>
-    )
-}
+          </div>
+          {progress !== null ? (
+            <CourseProgress
+              value={progress}
+              variant={progress === 100 ? "success" : "default"}
+              size="sm"
+            />
+          ) : (
+            <p className="text-md md:text-sm font-medium text-slate-700">
+              {formatPrice(price)}
+            </p>
+          )}
+        </div>
+      </div>
+    </Link>
+  );
+};
