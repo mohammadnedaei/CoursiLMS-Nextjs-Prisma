e7c678989de456242f0dab61d6cb37d795482cbc
mohammadnedaei <nedaei2002@gmail.com>
2024-08-21 20:04:52 +0330

    refactor(meta): refactor meta data

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
-import {authMiddleware} from "@clerk/nextjs";
+import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-    publicRoutes: ["/home", "/api/uploadthing", "/api/webhook"]
+  publicRoutes: ["/home", "/api/uploadthing", "/api/webhook"],
 });
 
 export const config = {
-    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
